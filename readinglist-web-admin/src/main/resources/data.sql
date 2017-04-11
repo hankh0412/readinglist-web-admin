938b5c2fe100787a25d53c73e3d22b6247d828af
louis <louis@louis-HP-Notebook>
2017-04-12 07:42:05 +0900

    test

--- readinglist-web-admin/src/main/resources/data.sql
+++ readinglist-web-admin/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-#insert into Reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
-#insert into Reader (username, password, fullname) values ('louis', 'password', 'Louis Han');
+#insert into reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
+#insert into reader (username, password, fullname) values ('louis', 'password', 'Louis Han');
 
