71f4ce114b9cba320924f8ce966125165c081e27
ohgamja3 <ohgamja3@gmail.com>
2019-01-21 11:28:57 +0900

    Changed folder structure

--- readinglist-web-admin/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-#insert into reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
-#insert into reader (username, password, fullname) values ('louis', 'password', 'Louis Han');
-#insert into reader (username, password, fullname) values ('dung', 'password', 'Dung Mom');
-
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+#insert into reader (username, password, fullname) values ('craig', 'password', 'Craig Walls');
+#insert into reader (username, password, fullname) values ('louis', 'password', 'Louis Han');
+#insert into reader (username, password, fullname) values ('dung', 'password', 'Dung Mom');
+
